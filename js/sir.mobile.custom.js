7e4b08a0155e8cbe8205c9ce0096746b6115df7b
lixianglin <lixianglin@b3cf33fc-efb8-45f3-beb8-482c4bfed20d>
2015-03-02 06:09:10 +0000

    add api and qunit

--- js/sir.mobile.custom.js
+++ js/sir.mobile.custom.js
@@ -30,3 +30,183 @@ function getContacts()
 
     return(json_str);
 }
+
+
+
+
+//--------------- USER ----------------
+function User()
+{
+	User.username = "";
+}
+
+User.login = function(username, password)
+{
+	User.username = username;
+	return true;
+}
+
+User.logout = function()
+{
+	User.username = "";
+	return true;
+}
+
+//--------------- ADDRESS ---------------
+
+function Address(){}
+Address.prototype.sync = function()
+{
+	return true;
+}
+
+Address.prototype.all = function()
+{
+	var obj = [
+		{
+			"subdepts": [
+				{
+					"subdepts": [],
+					"name": "行政部门",
+					"members": [
+						{
+							"sex": "女",
+							"phone": [
+								"010-88880001-0008"
+							],
+							"name": "蔡三",
+							"title": "财务3",
+							"mobile": [
+								"13800138008"
+							],
+							"email": [
+								"c1@email.com"
+							]
+						}
+					]
+				}
+			],
+			"name": "全体人员",
+			"members": []
+		}
+	];
+	
+	return obj;
+}
+
+
+//--------------- NOTICE ---------------
+function Notice(){}
+Notice.prototype.sync = function()
+{
+	return true;
+} 
+
+ Notice.prototype.all = function()
+ {
+	return [{"id":"100", "title":"测试", "time":"2015-01-27 11:20:20", "attachmentName":"1.jpg"}];
+ }
+ 
+ Notice.prototype.get = function(id)
+ {
+	if(id = 100)
+	{
+		return {"id":"100", "title":"测试", "time":"2015-01-27 11:20:20", "attachmentName":"1.jpg", "content":"测试内容"}
+	}
+	else
+	{
+		return null;
+	}
+ }
+ 
+ //--------------- FILE ---------------
+ 
+ function File(){}
+ File.prototype.list = function (path)
+ {
+	switch(path)
+	{
+		case "/":
+			return ["/cloud", "/"+User.username];
+		
+		case "/cloud":
+			return [];
+			
+		case "/"+User.username:
+			return [];
+			
+		default:
+			return [];
+	}
+ }
+ 
+//--------------- ATTENDANCE ---------------
+function Attendance(){}
+Attendance.prototype.checkin = function(areaLocation)
+{
+	return true;
+}
+
+ //--------------- MAIL ---------------
+ function Mail(){}
+ Mail.prototype.syncFolders = function()
+ {
+	//[{"id":"1000", "name":"INBOX"}]
+	return true;
+ }
+ 
+ Mail.prototype.syncMails = function(lastMailId)
+ {
+	return true;
+ }
+ 
+ Mail.prototype.getMailData = function(id)
+ {
+	var obj = {
+		"id":"100",
+		"from":"lishujun@qq.com",
+		"to":"58135482@qq.com",
+		"subject":"test",
+		"time":"2014-01-12 12:00:00",
+		"attachment":[{"name":"1.jpg", "url":"http://www.csdn.net/1.jpg"}],
+		"content":"sdfskajdfskdjfsdfj",
+	}
+	
+	if(id == 100)
+		return obj;
+	
+	return null;
+ }
+ 
+ Mail.prototype.send = function(info)
+ {
+	return true
+ }
+ 
+ Mail.prototype.getFolderList = function()
+ {
+	return [{"id":"100", "name":"TEST"}];
+ }
+ 
+ Mail.prototype.getMailList = function(folderId, pageSize, pageNo)
+ {
+	 
+	var obj = {
+		"id":"100",
+		"from":"lishujun@qq.com",
+		"to":"58135482@qq.com",
+		"subject":"test",
+		"time":"2014-01-12 12:00:00",
+		"attachment":[{"name":"1.jpg", "url":"http://www.csdn.net/1.jpg"}],
+	}
+	
+	if(folderId == 100 && pageSize == 10 && pageNo == 1)
+		return [obj];
+	
+	return [];
+ }
+ 
+ Mail.prototype.clean = function()
+ {
+	return true;
+ }
---
+++ js/tests.js
@@ -0,0 +1,153 @@
+﻿
+//--------------- USER ----------------
+QUnit.module( "User", {
+	beforeEach: function() {
+		
+	},
+  
+	afterEach: function(){
+	}
+});
+
+QUnit.test( "login", function( assert ) {
+	//result = login("lishujun", "123456");
+	result = User.login("lishujun", "123456");
+	assert.ok( result == true, "Passed!" );
+	
+	alert(User.username)
+	//result = logout();
+	assert.ok( result == true, "Passed!" );
+});
+
+//--------------- ADDRESS ---------------
+QUnit.module( "Address", {
+	beforeEach: function() {
+		address = new Address();
+	},
+  
+	afterEach: function(){
+	}
+});
+
+QUnit.test( "sync", function( assert ) {
+	result = address.sync()
+	assert.ok( result == true, "Passed!" );
+});
+
+QUnit.test( "all", function( assert ) {
+	arr = address.all()
+	deptname = arr[0].subdepts[0].name;
+	assert.ok( deptname == "行政部门", "Passed!" );
+	
+	employeeName = deptname = arr[0].subdepts[0].members[0].name;
+	assert.ok( employeeName == "蔡三", "Passed!" );
+	
+});
+
+//--------------- NOTICE ---------------
+QUnit.module( "Notice", {
+	beforeEach: function() {
+		notice = new Notice();
+	},
+  
+	afterEach: function(){
+	}
+});
+
+QUnit.test( "sync", function( assert ) {
+	result = notice.sync();
+	assert.ok( result == true, "Passed!" );
+});
+
+QUnit.test( "all", function( assert ) {
+	notices = notice.all();
+	assert.ok( notices.length > 0, "Passed!" );
+});
+
+QUnit.test( "get", function( assert ) {
+	notice = notice.get(100);
+	assert.ok( notice != null, "Passed!" );
+});
+
+ //--------------- FILE ---------------
+QUnit.module( "File", {
+	beforeEach: function() {
+		file = new File()
+	},
+  
+	afterEach: function(){
+	}
+});
+
+QUnit.test( "list", function( assert ) {
+	arr = file.list("/");
+	assert.ok(arr.length == 2, "Passed!")
+	for(i in arr)
+	{
+		path = arr[i];
+		file.list(path);
+	}
+});
+
+//--------------- ATTENDANCE ---------------
+QUnit.module( "Attendance", {
+	beforeEach: function() {
+		attendance = new Attendance()
+	},
+  
+	afterEach: function(){
+	}
+});
+
+QUnit.test( "checkin", function( assert ) {
+	result = attendance.checkin({"x":32.0, "y":44.4});
+	assert.ok( result == true, "Passed!" );
+});
+
+//--------------- MAIL ---------------
+QUnit.module( "Mail", {
+	beforeEach: function() {
+		mail = new Mail();
+	},
+  
+	afterEach: function(){
+	}
+});
+
+QUnit.test( "syncFolders", function( assert ) {
+	result = mail.syncFolders()
+	assert.ok( result == true, "Passed!" );
+});
+
+QUnit.test( "syncMails", function( assert ) {
+	result = mail.syncMails()
+	assert.ok( result == true, "Passed!" );
+});
+
+QUnit.test( "getMail", function( assert ) {
+	data = mail.getMailData()
+	assert.ok( 1 == 1, "Passed!" );
+});
+
+QUnit.test( "send", function( assert ) {
+	result = mail.send({})
+	assert.ok( result == true, "Passed!" );
+});
+
+QUnit.test( "getFolderList", function( assert ) {
+	list = mail.getFolderList()
+	assert.ok( 1 == 1, "Passed!" );
+});
+
+QUnit.test( "getMailList", function( assert ) {
+	list = mail.getMailList()
+	assert.ok( 1 == 1, "Passed!" );
+	
+});
+
+QUnit.test( "clean", function( assert ) {
+	result = mail.clean()
+	assert.ok( result == true, "Passed!" );
+});
+
+
