fad3e2ad5e9cc7f50eb1bdc55c6257e04b83c38f
lixianglin <lixianglin@b3cf33fc-efb8-45f3-beb8-482c4bfed20d>
2015-03-17 03:52:35 +0000

    api demo修改

--- js/sir.mobile.custom.js
+++ js/sir.mobile.custom.js
@@ -1,9 +1,6 @@
 ﻿
 // SIR Custom JS
 
-/*
-
-
 function getNotices()
 {
     var json_str = "[{\"id\":1}]";
@@ -30,7 +27,6 @@ function getContacts()
 
     return(json_str);
 }
-*/
 
 
 
@@ -64,30 +60,16 @@ Address.prototype.sync = function()
 
 Address.prototype.all = function()
 {
-    var json_str = "[{'subdepts': [{'subdepts': [], 'name': '\u884c\u653f\u90e8\u95e8', 'members': [{'qq': 123001, 'loginname': 'x1', 'name': '\u90a2\u4e00', 'title': '\u884c\u653f1', 'mobile': ['13800138001'], 'msn': '', 'sex': '\u7537', 'phone': ['010-88880001-0001'], 'wechat': 'ABCD001', 'password': '123qwe', 'email': ['x1@email.com']}, {'qq': 123002, 'loginname': 'x2', 'name': '\u90a2\u4e8c', 'title': '\u884c\u653f2', 'mobile': ['13800138002'], 'msn': '', 'sex': '\u7537', 'phone': ['010-88880001-0002'], 'wechat': 'ABCD002', 'password': '123qwe', 'email': ['x2@email.com']}, {'qq': 123003, 'loginname': 'x3', 'name': '\u90a2\u4e09', 'title': '\u884c\u653f3', 'mobile': ['13800138003'], 'msn': '', 'sex': '\u7537', 'phone': ['010-88880001-0003'], 'wechat': 'ABCD003', 'password': '123qwe', 'email': ['x3@email.com']}]}, {'subdepts': [], 'name': '\u4eba\u529b\u90e8\u95e8', 'members': [{'qq': 123004, 'loginname': 'r1', 'name': '\u4efb\u4e00', 'title': '\u4eba\u529b1', 'mobile': ['13800138004'], 'msn': '', 'sex': '\u7537', 'phone': ['010-88880001-0004'], 'wechat': 'ABCD004', 'password': '123qwe', 'email': ['r1@email.com']}, {'qq': 123005, 'loginname': 'r2', 'name': '\u4efb\u4e8c', 'title': '\u4eba\u529b2', 'mobile': ['13800138005'], 'msn': '', 'sex': '\u7537', 'phone': ['010-88880001-0005'], 'wechat': 'ABCD005', 'password': '123qwe', 'email': ['r2@email.com']}]}, {'subdepts': [], 'name': '\u8d22\u52a1\u90e8\u95e8', 'members': [{'qq': 123006, 'loginname': 'c1', 'name': '\u8521\u4e00', 'title': '\u8d22\u52a11', 'mobile': ['13800138006'], 'msn': '', 'sex': '\u5973', 'phone': ['010-88880001-0006'], 'wechat': 'ABCD006', 'password': '123qwe', 'email': ['c1@email.com']}, {'qq': 123007, 'loginname': 'c2', 'name': '\u8521\u4e8c', 'title': '\u8d22\u52a12', 'mobile': ['13800138007'], 'msn': '', 'sex': '\u5973', 'phone': ['010-88880001-0007'], 'wechat': 'ABCD007', 'password': '123qwe', 'email': ['c1@email.com']}, {'qq': 123008, 'loginname': 'c3', 'name': '\u8521\u4e09', 'title': '\u8d22\u52a13', 'mobile': ['13800138008'], 'msn': '', 'sex': '\u5973', 'phone': ['010-88880001-0008'], 'wechat': 'ABCD008', 'password': '123qwe', 'email': ['c1@email.com']}]}], 'name': '\u5168\u4f53\u4eba\u5458', 'members': []}]";
+    var json_str = "[{\"subdepts\": [{\"subdepts\": [], \"name\": \"\u884c\u653f\u90e8\u95e8\", \"members\": [{\"loginname\": \"x1\", \"sex\": \"\u7537\", \"phone\": [\"010-88880001-0001\"], \"wechat\": \"ABCD001\", \"password\": \"123qwe\", \"qq\": 123001, \"name\": \"\u90a2\u4e00\", \"title\": \"\u884c\u653f1\", \"mobile\": [\"13800138001\"], \"msn\": \"\", \"email\": [\"x1@email.com\"]}, {\"loginname\": \"x2\", \"sex\": \"\u7537\", \"phone\": [\"010-88880001-0002\"], \"wechat\": \"ABCD002\", \"password\": \"123qwe\", \"qq\": 123002, \"name\": \"\u90a2\u4e8c\", \"title\": \"\u884c\u653f2\", \"mobile\": [\"13800138002\"], \"msn\": \"\", \"email\": [\"x2@email.com\"]}, {\"loginname\": \"x3\", \"sex\": \"\u7537\", \"phone\": [\"010-88880001-0003\"], \"wechat\": \"ABCD003\", \"password\": \"123qwe\", \"qq\": 123003, \"name\": \"\u90a2\u4e09\", \"title\": \"\u884c\u653f3\", \"mobile\": [\"13800138003\"], \"msn\": \"\", \"email\": [\"x3@email.com\"]}]}, {\"subdepts\": [], \"name\": \"\u4eba\u529b\u90e8\u95e8\", \"members\": [{\"loginname\": \"r1\", \"sex\": \"\u7537\", \"phone\": [\"010-88880001-0004\"], \"wechat\": \"ABCD004\", \"password\": \"123qwe\", \"qq\": 123004, \"name\": \"\u4efb\u4e00\", \"title\": \"\u4eba\u529b1\", \"mobile\": [\"13800138004\"], \"msn\": \"\", \"email\": [\"r1@email.com\"]}, {\"loginname\": \"r2\", \"sex\": \"\u7537\", \"phone\": [\"010-88880001-0005\"], \"wechat\": \"ABCD005\", \"password\": \"123qwe\", \"qq\": 123005, \"name\": \"\u4efb\u4e8c\", \"title\": \"\u4eba\u529b2\", \"mobile\": [\"13800138005\"], \"msn\": \"\", \"email\": [\"r2@email.com\"]}]}, {\"subdepts\": [], \"name\": \"\u8d22\u52a1\u90e8\u95e8\", \"members\": [{\"loginname\": \"c1\", \"sex\": \"\u5973\", \"phone\": [\"010-88880001-0006\"], \"wechat\": \"ABCD006\", \"password\": \"123qwe\", \"qq\": 123006, \"name\": \"\u8521\u4e00\", \"title\": \"\u8d22\u52a11\", \"mobile\": [\"13800138006\"], \"msn\": \"\", \"email\": [\"c1@email.com\"]}, {\"loginname\": \"c2\", \"sex\": \"\u5973\", \"phone\": [\"010-88880001-0007\"], \"wechat\": \"ABCD007\", \"password\": \"123qwe\", \"qq\": 123007, \"name\": \"\u8521\u4e8c\", \"title\": \"\u8d22\u52a12\", \"mobile\": [\"13800138007\"], \"msn\": \"\", \"email\": [\"c1@email.com\"]}, {\"loginname\": \"c3\", \"sex\": \"\u5973\", \"phone\": [\"010-88880001-0008\"], \"wechat\": \"ABCD008\", \"password\": \"123qwe\", \"qq\": 123008, \"name\": \"\u8521\u4e09\", \"title\": \"\u8d22\u52a13\", \"mobile\": [\"13800138008\"], \"msn\": \"\", \"email\": [\"c1@email.com\"]}]}], \"name\": \"\u5168\u4f53\u4eba\u5458\", \"members\": []}]";
     
-    return json_str;
+    return(json_str);
 }
 
 Address.prototype.get = function(id)
 {
-    var json_str = {
-                "id": "user_c3"
-                "sex": "女",
-                "phone": [
-                    "010-88880001-0008"
-                ],
-                "name": "蔡三",
-                "title": "财务3",
-                "mobile": [
-                    "13800138008"
-                ],
-                "email": [
-                    "c1@email.com"
-                ]
-            };
+    var json_str = "{\"name\": \"\u8521\u4e09\", \"title\": \"\u8d22\u52a13\", \"mobile\": [\"13800138008\"], \"sex\": \"\u5973\", \"phone\": [\"010-88880001-0008\"], \"email\": [\"c3@email.com\"], \"id\": \"user_c3\"}";
 
-    return json_str;
+    return(json_str);
 }
 
 
@@ -100,14 +82,16 @@ Notice.prototype.sync = function()
 
  Notice.prototype.all = function()
  {
-    return [{"id":"100", "title":"测试", "time":"2015-01-27 11:20:20", "attachmentName":"1.jpg"}];
+    var json_str = "[{\"id\":\"100\", \"title\":\"\u6d4b\u8bd51\", \"time\":\"2015-01-27 11:20:20\", \"attachmentName\":\"1.jpg\"},{\"id\":\"101\", \"title\":\"\u6d4b\u8bd52\", \"time\":\"2015-01-27 11:20:20\", \"attachmentName\":\"2.jpg\"}]";
+    return(json_str);
  }
  
  Notice.prototype.get = function(id)
  {
     if(id = 100)
     {
-        return {"id":"100", "title":"测试", "time":"2015-01-27 11:20:20", "attachmentName":"1.jpg", "content":"测试内容"}
+        var json_str = "{\"id\":\"100\", \"title\":\"\u6d4b\u8bd5\", \"time\":\"2015-01-27 11:20:20\", \"attachmentName\":\"1.jpg\", \"content\":\"\u6d4b\u8bd5\u5185\u5bb9\"}";
+        return(json_str);
     }
     else
     {
@@ -138,9 +122,8 @@ Notice.prototype.sync = function()
  
 File.prototype.share = function (path)
 {
-    json_str = {
-        ""
-    }
+    var json_str = "{\"url\": \"http:\/\/sirself.sirbox.cn\/sir\/data\/public\/9c73dc.php\"}";
+    return(json_str);
 }
  
 //--------------- ATTENDANCE ---------------
@@ -153,18 +136,15 @@ Attendance.prototype.checkin = function(areaLocation)
 
 Attendance.prototype.checkout = function(areaLocation)
 {
-    var json_str = "{\"result\":\"false\", \"message\": \"超时\"}";
+    var json_str = "{\"result\":\"false\", \"message\": \"\u8d85\u65f6\"}";
     return(json_str);
 }
 
 Attendance.prototype.getInfo = function()
 {
     //没数据返回空记录。例如：{"cleckin":"","cleckout":""}
-    var json_str = {
-        "checkin":"2015-03-14 08:00:00",
-        "checkout":"2015-03-14 18:00:00"
-    }
-    return json_str;
+    var json_str = "{\"checkin\": \"2015-03-14 08:00:00\", \"checkout\": \"2015-03-14 18:00:00\"}";
+    return(json_str);
 }
 
  //--------------- MAIL ---------------
@@ -182,18 +162,10 @@ Attendance.prototype.getInfo = function()
  
  Mail.prototype.getMailData = function(id)
  {
-    var json_str = {
-        "id":"100",
-        "from":"lishujun@qq.com",
-        "to":"58135482@qq.com",
-        "subject":"test",
-        "time":"2014-01-12 12:00:00",
-        "attachment":[{"name":"1.jpg", "url":"http://www.csdn.net/1.jpg"}],
-        "content":"sdfskajdfskdjfsdfj",
-    }
+    var json_str = "{\"from\": \"lishujun@qq.com\", \"content\": \"sdfskajdfskdjfsdfj\", \"to\": \"58135482@qq.com\", \"attachment\": [{\"url\": \"http:\/\/www.csdn.net\/1.jpg\", \"name\": \"1.jpg\"}], \"time\": \"2014-01-12 12:00:00\", \"id\": \"100\", \"subject\": \"test\"}";
     
     if(id == 100)
-        return json_str;
+        return(json_str);
     
     return null;
  }
@@ -205,23 +177,17 @@ Attendance.prototype.getInfo = function()
  
  Mail.prototype.getFolderList = function()
  {
-    return [{"id":"100", "name":"TEST"}];
+    var json_str = "[{\"id\": \"100\", \"name\": \"\u6536\u4ef6\u7bb1\"}, {\"id\": \"101\", \"name\": \"\u53d1\u4ef6\u7bb1\"}, {\"id\": \"102\", \"name\": \"\u8349\u7a3f\u7bb1\"}, {\"id\": \"103\", \"name\": \"\u5783\u573e\u7bb1\"}, {\"id\": \"104\", \"name\": \"Redmine\"}]";
+    return(json_str);
  }
  
  Mail.prototype.getMailList = function(folderId, pageSize, pageNo)
  {
      
-    var json_str = {
-        "id":"100",
-        "from":"lishujun@qq.com",
-        "to":"58135482@qq.com",
-        "subject":"test",
-        "time":"2014-01-12 12:00:00",
-        "attachment":[{"name":"1.jpg", "url":"http://www.csdn.net/1.jpg"}],
-    }
+    var json_str = "[{\"from\": \"lishujun@qq.com\", \"content\": \"sdfskajdfskdjfsdfj\", \"to\": \"58135482@qq.com\", \"attachment\": [{\"url\": \"http:\/\/www.csdn.net\/1.jpg\", \"name\": \"1.jpg\"}], \"time\": \"2014-01-12 12:00:00\", \"id\": \"100\", \"subject\": \"test1\", \"nextMailId\": \"101\"},{\"from\": \"lishujun@qq.com\", \"content\": \"sdfskajdfskdjfsdfj\", \"to\": \"58135482@qq.com\", \"attachment\": [{\"url\": \"http:\/\/www.csdn.net\/1.jpg\", \"name\": \"2.jpg\"}], \"time\": \"2014-01-12 12:00:00\", \"id\": \"101\", \"subject\": \"test\", \"nextMailId\": \"102\"},{\"from\": \"lishujun@qq.com\", \"content\": \"sdfskajdfskdjfsdfj\", \"to\": \"58135482@qq.com\", \"attachment\": [{\"url\": \"http:\/\/www.csdn.net\/1.jpg\", \"name\": \"3.jpg\"}], \"time\": \"2014-01-12 12:00:00\", \"id\": \"102\", \"subject\": \"test\", \"nextMailId\": \"103\"}]";
     
     if(folderId == 100 && pageSize == 10 && pageNo == 1)
-        return [json_str];
+        return(json_str);
     
     return [];
  }
